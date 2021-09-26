f6416d305ec4e87952eb2764c9555cc8bbe24f3d
KKINIUSS <kisell22@yanex.ru>
2021-09-26 23:28:19 +0300

    26.09.21

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -3,5 +3,5 @@ from .waiting_handler import dp
 from .registration_handler import dp
 from .foreman_handler import dp
 from .worker_handler import dp
-from .manager_handler import dp
+#from .manager_handler import dp
 __all__ = ["dp"]
